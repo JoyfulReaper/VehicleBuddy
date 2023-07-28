6b80fbfd2bca6d219b19720a06a185e15902f355
Kyle Givler <xvegan88x@gmail.com>
2023-07-27 21:20:36 -0400

    was just missing the using.... doh

--- VehicleBuddy/VehicleBuddy/Client/Program.cs
+++ VehicleBuddy/VehicleBuddy/Client/Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using VehicleBuddy.Client;
+using VehicleBuddy.ApiClient;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-//builder.Services.AddVehicleBuddyClient // Im not sure why this still isnt showing up - will TS more later
+builder.Services.AddVehicleBuddyClient(builder.Configuration);
 
 await builder.Build().RunAsync();
