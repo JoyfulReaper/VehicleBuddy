73fc6a9634245f431ab21c039ef968c28768c409
Kyle Givler <xvegan88x@gmail.com>
2023-08-03 19:39:45 -0400

    Finish API Wrapper

---
+++ VehicleBuddy/VehicleBuddy.ApiClient/HttpClients/IVehicleClient.cs
@@ -0,0 +1,13 @@
+﻿using VehicleBuddy.Shared.Contracts.Vehicle;
+
+namespace VehicleBuddy.ApiClient.HttpClients;
+
+public interface IVehicleClient
+{
+    Task<List<VehicleResponse>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<VehicleResponse?> GetAsync(int id, CancellationToken cancellationToken = default);
+    Task<VehicleResponse?> GetByVin(string vin, CancellationToken cancellationToken = default);
+    Task<VehicleResponse> UpdateAsync(UpdateVehicleRequest vehicleRequest, CancellationToken cancellationToken = default);
+    Task<VehicleResponse> CreateAsync(CreateVehicleRequest vehicleRequest, CancellationToken cancellationToken = default);
+    Task DeleteAsync(int id, CancellationToken cancellationToken = default);
+}
